5d580997561d0ddd5731cc4774442c0a68faea29
Sashank030 <145583612+Sashank030@users.noreply.github.com>
2024-05-26 19:24:42 +0530

    Add files via upload

---
+++ backend/src/test/java/com/projectpeerfinder/ProjectPeerFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectpeerfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPeerFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
